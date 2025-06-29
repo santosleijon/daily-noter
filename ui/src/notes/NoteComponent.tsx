cec28721eb9da3bec634e9bbb5056e1a0cab93ea
Santos Leijon <santos.bostrom.leijon@gmail.com>
2025-06-29 20:23:01 +0200

    Show prettier date formats

--- ui/src/notes/NoteComponent.tsx
+++ ui/src/notes/NoteComponent.tsx
@@ -19,18 +19,52 @@ const NoteComponent = ({ note, onSave }: NoteComponentProps) => {
     setSaveIsLoading(false);
   };
 
+  function getWeekdayFromDateString(dateString: string): string {
+    const date = new Date(dateString);
+
+    if (isNaN(date.getTime())) {
+      throw Error("Invalid date: " + dateString);
+    }
+
+    const options: Intl.DateTimeFormatOptions = {
+      weekday: 'long',
+    };
+
+    const formatter = new Intl.DateTimeFormat('en-US', options);
+
+    return formatter.format(date);
+  }
+
+  function getPrettyDate(date: Date): string {
+    const options: Intl.DateTimeFormatOptions = {
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      hourCycle: 'h23'
+    };
+
+    const formatter = new Intl.DateTimeFormat('sv-SE', options);
+
+    return formatter.format(date);
+  }
+
+  const noteDate = `${getWeekdayFromDateString(note.date)}, ${note.date}`;
+
   return (
     <div>
-      <div className="mt-8 flex items-center">
-        <h3>{note.date}</h3>
-        <div className="text-xs ml-2 mb-1 text-cyan-800">
-          Created {note.createdAt.toISOString()}{note.updatedAt && <>, updated {note.updatedAt.toISOString()}</>}
-        </div>
+      <div>
+        <h3 className="mt-8">{noteDate}</h3>
+      </div>
+      <div className="text-xs mb-2 text-cyan-800">
+        <p>Created {getPrettyDate(note.createdAt)} {note.updatedAt &&
+          <>(updated {getPrettyDate(note.updatedAt)})</>}</p>
       </div>
       <form onSubmit={handleSubmit}>
         <textarea
           className="block mb-3 p-2 w-full text-sm text-gray-900 rounded border-solid field-sizing-content overflow-hidden"
-          rows={5}
+          rows={10}
           onChange={(e) => setContent(e.target.value)}
           defaultValue={note.content} />
         <LoadableSubmitButton isLoading={saveIsLoading} text="Save" />
