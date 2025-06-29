bacbb03c3a60f0913d6784063a0d385ef8bb3e93
Santos Leijon <santos.bostrom.leijon@gmail.com>
2025-06-29 21:23:16 +0200

    Adjust cyan button color

--- ui/src/common/LoadableSecondaryButton.tsx
+++ ui/src/common/LoadableSecondaryButton.tsx
@@ -12,7 +12,7 @@ const LoadableSecondaryButton = (props: LoadableSecondaryButtonProps) => {
     disabled={props.isLoading}
     type="button"
     onClick={props.onClick}
-    className="text-white bg-cyan-500 hover:bg-blue-600 cursor-pointer focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 inline-flex items-center">
+    className="text-white bg-cyan-500 hover:bg-cyan-600 cursor-pointer focus:ring-4 focus:ring-cyan-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 inline-flex items-center">
     {props.isLoading && (
       <LoadingSpinner />
     )}
